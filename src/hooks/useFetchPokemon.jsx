4a1c09f39b974128acc35dd67b83291f858b09e4
Mayra Landa <mlandae16@gmail.com>
2024-07-29 15:03:30 -0700

    add pokemon fetch

---
+++ src/hooks/useFetchPokemon.jsx
@@ -0,0 +1,25 @@
+const api = "https://pokeapi.co/api/v2/pokemon";
+
+export const fetchPokemon = async (pokemonId) => {
+  const response = await fetch (`${api}/${pokemonId}`);
+  const pokemonJson = await response.json();
+  return pokemonJson;
+};
+
+export const fetchPokemonData = async (num = 8) => {
+  const pokemonPromises = [];
+  for (let i = 1; i <= num; i++){
+    const randomId = Math.floor(Math.random() * 151) + 1;
+    pokemonPromises.push(fetchPokemon(randomId));
+  }
+
+  const pokemonData = await Promise.all(pokemonPromises);
+  console.log("Fetched Pokemon Data:", pokemonData);
+  return pokemonData.map(pokemon => ({
+    id: pokemon.id,
+    image: pokemon.sprites.font_default,
+  }))
+}
+
+
+
